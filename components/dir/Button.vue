600ea934776b85fa291d0e0cc85955cd9995edde
codehzy <1257664828@qq.com>
2022-11-11 15:53:08 +0800

    feat(完成基本): ✨ 完成基本

---
+++ components/dir/Button.vue
@@ -0,0 +1,7 @@
+<template>
+  <button>我是一个按钮公共组件</button>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped></style>
---
+++ packages/web/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { getUser } from "@zy/api";
+import Button from "@zy/components";
+</script>
+
+<template>
+  <div>
+    <Button></Button>
+  </div>
+</template>
+
+<style scoped></style>
